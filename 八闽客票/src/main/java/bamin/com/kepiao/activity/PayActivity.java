7c02905b69ad1ed840f8290af1890f4fea0a958e
zjb <zjb@aiton.com.cn>
2016-06-07 14:21:44 +0800

    删除无用代码2016年6月7日14:21:24

--- 八闽客票/src/main/java/bamin/com/kepiao/activity/PayActivity.java
+++ 八闽客票/src/main/java/bamin/com/kepiao/activity/PayActivity.java
@@ -50,7 +50,6 @@ import java.util.Random;
 
 import bamin.com.kepiao.R;
 import bamin.com.kepiao.Zalipay.PayResult;
-import bamin.com.kepiao.Zalipay.SignUtils;
 import bamin.com.kepiao.constant.Constant;
 import bamin.com.kepiao.models.Sign;
 import bamin.com.kepiao.models.about_order.QueryOrder;
@@ -76,7 +75,7 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
     private RadioGroup mPay_mode;
     private RadioButton mRadioButton_zhifubao;
     private RadioButton mRadioButton_weixin;
-    private String payMode = "支付宝";//支付方式
+    private String payMode = "微信";//支付方式
     private RelativeLayout mRela_useTheRedBag;
     private TextView mTextView_redBagCount;
     private RedPacket mRedBag;
@@ -86,14 +85,6 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
     private Map<String, String> mGetWechatOrderParams;
     private Runnable mR;
     private String mId;
-    // 商户PID
-    public static final String PARTNER = "2088121765668893";
-    // 商户收款账号
-    public static final String SELLER = "fj_bmcx@163.com";
-    // 商户私钥，pkcs8格式
-    public static final String RSA_PRIVATE = "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBALCKLf3VXqSQX3ABi8eAar58zVgQ6AjI7egEWaXLiJ+Dp2ZBGWom1d0LS3Ybdo3mgsCAQS/duufPzWPoPDVllTFk+2B+zfeTuXj7kDo3fVYt6laB07E0iijzMPFHSuMCFuqlS06dSSnzwzUpQ6fohVdyftoXzZyidFWAhJgrInUvAgMBAAECgYEAgtRkcRCHKv2zPJZouFh6wuBKbVFaI+iEJsXSigPkJBK1JBpqcziElWOhcM25dj+19mMV7BsuQEzHsZqRxCCcBtvu5+DztX1l8KmYpqdyccQ4m/oEVcVneWRDmt419+Tm2I7YEyoXERC6Gh1Dypw0A5SJdLibEfFeKoHEnoHzzkECQQDU4nQqjBu4uXbpfrjXKfUZN8+k5u/+4Yhy3VZOLUvmTQjqZVKeirGImjrddiRp56sFrmsZhanqK1UKieayI5rrAkEA1EtV6vIV9OpCgVeLaWLOYvK0eX3loiDlQc7IIAgt18dddlSUYLATFzrMt1dnEteyiVXNphwJwx8mqkWU4/Z1zQJAe1QhtlBq8u5HDGhEjyoYex6RJdhAKynfUaQWjr3BHc99HcXLQlvZE+k9tvTtjYkP0//Cvgtob2fhIXTYeFUWNwJAFOtfiglU9I9pAknYKQhdgg6cjiRDzpgjPzrbKZzkt5CjuxdWj7iKCQ34QlDQjWDH7RSRcT7uD1YwfzLgGx6cOQJAQ1tOCyvjcnetnCTNDOeCwaZF2LHtUWAnp6MyPE1Yq12RbjG0q6ai23a5tSWoHfCqHuA/77XXAShhj5dnQMHmRw==";
-    // 支付宝公钥
-    public static final String RSA_PUBLIC = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDDI6d306Q8fIfCOaTXyiUeJHkrIvYISRcc73s3vF1ZT7XN8RNPwJxo8pWaJMmvyTn9N4HQ632qJBVHf8sxHi/fEsraprwCtzvzQETrNRwVxLO5jVmRGi60j8Ue1efIlzPXV9je9mkjzOmdssymZkh2QhUrCmZYI/FCEa3/cNMW0QIDAQAB";
     private static final int SDK_PAY_FLAG = 1;
     private static final int SDK_CHECK_FLAG = 2;
     private AlertDialog mWechatPayAlertDialog;
@@ -427,6 +418,7 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
         {
             if (mRadioButton_zhifubao.isChecked())
             {
+                Log.e("onCheckedChanged", "支付宝");
                 payMode = "支付宝";
             } else if (mRadioButton_weixin.isChecked())
             {
@@ -470,50 +462,7 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
                 confrimOrder();
             } else
             {
-//                String orderInfo = getOrderInfo(mQueryOrder.getStartSiteName() + "-" + mQueryOrder.getEndSiteName(), "车票信息", realPayPrice + "");
-//
-//                /**
-//                 * 特别注意，这里的签名逻辑需要放在服务端，切勿将私钥泄露在代码中！
-//                 */
-//                String sign = sign(orderInfo);
                 getSign();
-//                try
-//                {
-//                    /**
-//                     * 仅需对sign 做URL编码
-//                     */
-//                    sign = URLEncoder.encode(sign, "UTF-8");
-//                } catch (UnsupportedEncodingException e)
-//                {
-//                    e.printStackTrace();
-//                }
-//
-//                /**
-//                 * 完整的符合支付宝参数规范的订单信息
-//                 */
-//                final String payInfo = orderInfo + "&sign=\"" + sign + "\"&" + getSignType();
-//                Log.e("pay", "正确签名" + payInfo);
-//                Runnable payRunnable = new Runnable()
-//                {
-//
-//                    @Override
-//                    public void run()
-//                    {
-//                        // 构造PayTask 对象
-//                        PayTask alipay = new PayTask(PayActivity.this);
-//                        // 调用支付接口，获取支付结果
-//                        String result = alipay.pay(payInfo, true);
-//
-//                        Message msg = new Message();
-//                        msg.what = SDK_PAY_FLAG;
-//                        msg.obj = result;
-//                        mHandler.sendMessage(msg);
-//                    }
-//                };
-//
-//                // 必须异步调用
-//                Thread payThread = new Thread(payRunnable);
-//                payThread.start();
             }
 
         } else
@@ -524,8 +473,7 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
     }
 
     private void getSign() {
-//        String url = Constant.Url.GETSIGN;
-        String url = "http://192.168.1.108:8080/app/alipay/getsign";
+        String url = Constant.Url.GETSIGN;
         Map<String, String> map = new HashMap<>();
         map.put("out_trade_no",getOutTradeNo());
         map.put("subject", mQueryOrder.getStartSiteName() + "-" + mQueryOrder.getEndSiteName());
@@ -566,60 +514,6 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
         });
     }
 
-    /**
-     * create the order info. 创建订单信息
-     */
-    private String getOrderInfo(String subject, String body, String price)
-    {
-
-        // 签约合作者身份ID
-        String orderInfo = "partner=" + "\"" + PARTNER + "\"";
-
-        // 签约卖家支付宝账号
-        orderInfo += "&seller_id=" + "\"" + SELLER + "\"";
-
-        // 商户网站唯一订单号
-        orderInfo += "&out_trade_no=" + "\"" + getOutTradeNo() + "\"";
-
-        // 商品名称
-        orderInfo += "&subject=" + "\"" + subject + "\"";
-
-        // 商品详情
-        orderInfo += "&body=" + "\"" + body + "\"";
-
-        // 商品金额
-        orderInfo += "&total_fee=" + "\"" + price + "\"";
-
-        // 服务器异步通知页面路径
-        orderInfo += "&notify_url=" + "\"" + "http://120.24.46.15:8080/aiton-tickets-app-webapp/alipay/notify" + "\"";
-
-        // 服务接口名称， 固定值
-        orderInfo += "&service=\"mobile.securitypay.pay\"";
-
-        // 支付类型， 固定值
-        orderInfo += "&payment_type=\"1\"";
-
-        // 参数编码， 固定值
-        orderInfo += "&_input_charset=\"utf-8\"";
-
-        // 设置未付款交易的超时时间
-        // 默认30分钟，一旦超时，该笔交易就会自动被关闭。
-        // 取值范围：1m～15d。
-        // m-分钟，h-小时，d-天，1c-当天（无论交易何时创建，都在0点关闭）。
-        // 该参数数值不接受小数点，如1.5h，可转换为90m。
-        orderInfo += "&it_b_pay=\"30m\"";
-
-        // extern_token为经过快登授权获取到的alipay_open_id,带上此参数用户将使用授权的账户进行支付
-        // orderInfo += "&extern_token=" + "\"" + extern_token + "\"";
-
-        // 支付宝处理完请求后，当前页面跳转到商户指定页面的路径，可空
-        orderInfo += "&return_url=\"m.alipay.com\"";
-
-        // 调用银行卡支付，需配置此参数，参与签名， 固定值 （需要签约《无线银行卡快捷支付》才能使用）
-        // orderInfo += "&paymethod=\"expressGateway\"";
-
-        return orderInfo;
-    }
 
     /**
      * get the out_trade_no for an order. 生成商户订单号，该值在商户端应保持唯一（可自定义格式规范）
@@ -635,25 +529,6 @@ public class PayActivity extends AppCompatActivity implements View.OnClickListen
         key = key.substring(0, 15);
         return key;
     }
-
-    /**
-     * sign the order info. 对订单信息进行签名
-     *
-     * @param content 待签名订单信息
-     */
-    private String sign(String content)
-    {
-        return SignUtils.sign(content, RSA_PRIVATE);
-    }
-
-    /**
-     * get the sign type we use. 获取签名方式
-     */
-    private String getSignType()
-    {
-        return "sign_type=\"RSA\"";
-    }
-    /*-----------------------支付宝End-----------------------*/
 
     //支付成功dialog提示
     private void setSuccessDialog(String messageTxt, String iSeeTxt)
